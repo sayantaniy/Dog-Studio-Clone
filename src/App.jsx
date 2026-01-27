1d17d4afda22d397677973028ca10cfa90f74172
sayantaniy <sayantanimanna139@gmail.com>
2026-01-27 15:27:47 +0530

    added animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
 import Dog from "./components/Dog";
 import { Canvas } from "@react-three/fiber";
-import "./App.css";
+import "./index.css";
+
 
 const App = () => {
   return (
-    <div style={{ width: "100%", height: "100vh" }}>
-      <Canvas>
+    <div >
+      <main>
+      <Canvas style={{
+        height:'100vh',
+        width:'100vw',
+        position:'fixed',
+        top:0,
+        left:0,
+        zIndex:'1',
+        backgroundImage: 'url(/public/1.png)',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover'
+
+      }}>
         <Dog />
       </Canvas>
+      <section id="section1"></section>
+      <section id="section2"></section>
+      <section id="section3"></section>
+      </main>
     </div>
   );
 };
